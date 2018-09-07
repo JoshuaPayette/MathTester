2f7dd469c5593579da3829636f7226a397bd28e4
JoshuaPayette <42836647+JoshuaPayette@users.noreply.github.com>
2018-09-07 07:29:38 -0400

    removed block comments

--- src/MathTester.java
+++ src/MathTester.java
@@ -31,14 +31,14 @@ public class MathTester {
         What is your height in inches? 69
         inches = their input*/
 
-       /* System.out.println(firstWord+secondWord);
+        System.out.println(firstWord+secondWord);
         System.out.println(firstWord+(firstNumber+secondNumber));
         System.out.println("firstNumber + secondNumber is "+ (firstNumber+secondNumber));
         System.out.println("firstNumber - secondNumber is "+ (firstNumber-secondNumber));
         System.out.println("secondNumber / firstNumber is "+ (secondNumber/firstNumber));
         System.out.println("firstNumber * secondNumber is "+ (firstNumber*secondNumber));
         System.out.println("The modulus of the secondNumber by the firstNumber" +
-                " is "+ (secondNumber%firstNumber));*/
+                " is "+ (secondNumber%firstNumber));
 
 
 
